8bf8ecdb9eb525cef594ff7dc1d363dacb02ad49
Anannyakandhari <anannyakandhari@gmail.com>
2025-11-02 12:50:03 -0600

    css file

--- script.js
+++ script.js
@@ -4,37 +4,46 @@ console.log("started");
 // asynchronous function (because it gets data from an API)
 async function getTrees() {
     // always await fetch
+ try {
     const response = await fetch("https://data.winnipeg.ca/resource/hfwk-jp4h.json");
-    
+    if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
     // parse to json
     // always await response.json()
     const jsonResponse = await response.json();
     return jsonResponse;
+}
+catch (error) {
+        console.error("Error fetching tree data:", error);
+        alert("Failed to load tree data. Please try again later.");
+    }
 }
 
 // 2. Parse the data that I get as a response to find what I need
 function getTreesInfo(data) {
     // start with empty array 
     const allTrees = [];
-
-    data.forEach(tree => {
-        const treesDetails = {
-            commonName: tree["common_name"],
-            botanicalName: tree["botanical_name"],
-            diameter: tree["diameter_at_breast_height"],
-            park: tree["park"],
-            neighbourhood: tree["neighbourhood"]
-        };
-        allTrees.push(treesDetails);
+data.forEach(tree => {
+        const { common_name, botanical_name, diameter_at_breast_height, park, neighbourhood } = tree;
+        allTrees.push({
+            commonName: common_name,
+            botanicalName: botanical_name,
+            diameter: diameter_at_breast_height,
+            park,
+            neighbourhood
+        });
     });
 
+    
     return allTrees;
 }
+
 
 window.onload = async() => {
     // wait for getCountries() to finish before going to the next line
     // always await a function that does a fetch
-    const Data = await getTress();
+    const Data = await getTrees();
     console.log(getTreesInfo(Data));
 }
  
